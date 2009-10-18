22aced59334f4786cab15228438493c8a2fc7956
zalun <piotr@zalewa.info>
2009-10-18 22:19:13 +0100

    removed unused files

--- params.py
+++
@@ -1,7 +0,0 @@
-from django.conf import settings   
-
-MOOTOOLS_CORE = "js/lib/mootools-1.2.3-core-yc.js"
-MOOTOOLS_DEV_CORE = "js/lib/mootools-1.2.3-core-nc.js"
-MOOTOOLS_MORE = "js/lib/mootools-1.2.3.1-more.js"
-MOOTOOLS_DEPENDER = "js/lib/mootools-more/Source/Core/Depender.js"
-SLUG_LENGTH = 5
