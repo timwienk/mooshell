18e76dba4fdf73e539ca2393b497378cf163ba42
Tim Wienk <timwienk@gmail.com>
2009-12-20 23:16:30 +0100

    Added editor window resizing functionality.
    Implemented through More's Drag class.

--- media/js/LayoutCM.js
+++ media/js/LayoutCM.js
@@ -48,6 +48,8 @@ var Layout = {
 
 		this.resize();
 
+		this.createDragInstances();
+
 		this.fireEvent('ready');
 	},
 	findLayoutElements: function() {
@@ -55,6 +57,12 @@ var Layout = {
 		this.content = document.id('content');
 		this.columns = this.content.getChildren('.column');
 		this.windows = this.content.getElements('.window');
+		this.shims = this.content.getElements('.column .shim');
+		this.handlers = $H({
+			'vertical': this.content.getElementById('handler_vertical'),
+			'left': this.columns[0].getElement('.handler_horizontal'),
+			'right': this.columns[1].getElement('.handler_horizontal')
+		});
 	},
 	registerEditor: function( editor ) {
 		this.editors[editor.options.name] = editor;
@@ -69,6 +77,35 @@ var Layout = {
 		this.editors.each( function(ed) {
 			ed.clean();
 		});
+	},
+	createDragInstances: function() {
+		var onDrag_horizontal = function(h) {
+			var windows = h.getParent().getElements('.window');
+			var top = (h.getPosition(this.content).y + h.getHeight() / 2) / this.content.getHeight() * 100;
+			windows[0].setStyle('height', top + '%');
+			windows[1].setStyle('height', 100 - top + '%');
+		}.bind(this);
+
+		var onDrag_vertical = function(h) {
+			var left = (h.getPosition(this.content).x + h.getWidth() / 2) / this.content.getWidth() * 100;
+			this.columns[0].setStyle('width', left + '%');
+			this.columns[1].setStyle('width', 100 - left + '%');
+		}.bind(this);
+
+		this.handlers.each(function(h) {
+			var isHorizontal = h.hasClass('handler_horizontal');
+			h.dragInstance = new Drag(h, {
+				'modifiers': isHorizontal ? {'x': null, 'y': 'top'} : {'x': 'left', 'y': null},
+				'limit': {
+					'x': [100, this.content.getWidth() - 100],
+					'y': [100, this.content.getHeight() - 100]
+				},
+				'onBeforeStart': function() { this.shims.show(); }.bind(this),
+				'onDrag': isHorizontal ? onDrag_horizontal : onDrag_vertical,
+				'onCancel': function() { this.shims.hide(); }.bind(this),
+				'onComplete': function() { this.shims.hide(); }.bind(this)
+			});
+		}, this);
 	},
 	resize: function(e) {
 
@@ -90,6 +127,11 @@ var Layout = {
 						this.windows[1].getStyle('bottom').toInt();
 
 		this.content.setStyle('height', av_height);
+
+		// set handler positions
+		this.handlers.vertical.setStyle('left', this.windows[0].getCoordinates(this.content).right);
+		this.handlers.left.setStyle('top', this.windows[0].getCoordinates(this.content).bottom);
+		this.handlers.right.setStyle('top', this.windows[2].getCoordinates(this.content).bottom);
 
 //		this.editors.each( function(ed) {
 //			ed.show();
